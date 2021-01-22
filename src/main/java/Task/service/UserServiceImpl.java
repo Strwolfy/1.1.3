64c104335f1add211ad9f714bd6d770fcd67177c
Mikhail <strwolfy@gmail.com>
2021-01-22 12:42:54 +0300

    Сделано!

--- src/main/java/Task/service/UserServiceImpl.java
+++ src/main/java/Task/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package Task.service;
 import Task.dao.UserDaoJDBCImpl;
 import Task.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService  {
@@ -17,11 +18,11 @@ public class UserServiceImpl implements UserService  {
         daoJDBC.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         daoJDBC.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         daoJDBC.removeUserById(id);
     }
 
